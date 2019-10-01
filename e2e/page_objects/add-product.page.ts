3dff38d7f996d1a8d2ae101b06e2f74c51424d26
student <student@safebear.co.uk>
2019-10-01 14:23:26 +0100

    home page

---
+++ e2e/page_objects/add-product.page.ts
@@ -0,0 +1,6 @@
+import { $, by, element } from "protractor";
+
+export class Angular7Crud {
+
+}
+
---
+++ e2e/page_objects/home.page.ts
@@ -0,0 +1,5 @@
+import { $, by, element } from "protractor";
+
+export class HomePage {
+public addProductButton = $('.mat-flat-button.mat-primary');
+}
