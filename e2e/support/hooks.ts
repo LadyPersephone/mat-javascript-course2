5515b8d9b2b33501e34459f4309cb4b9fb83ee6c
student <student@safebear.co.uk>
2019-10-01 12:09:26 +0100

    update hooks

--- e2e/support/hooks.ts
+++ e2e/support/hooks.ts
@@ -5,6 +5,6 @@ import { browser } from "protractor";
 Before({timeout: 100 * 1000}, async function() {
     
     // Opens the website to the default URL in the 'protractor.config.ts' file await browser.get("");
-    await browser
+    await browser.get("");
     
 });
