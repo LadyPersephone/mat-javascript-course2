48284a347ef45ca7a601dc7933421a2cddd1af5f
student <student@safebear.co.uk>
2019-10-01 11:27:49 +0100

    update World

--- e2e/support/productsListWorld.ts
+++ e2e/support/productsListWorld.ts
@@ -1,6 +1,10 @@
 import {World} from "cucumber";
 
 declare module "cucumber" {
+
+    // Enforces the format of our test data
+    
+    // A template for 'product' test data
     interface World {
         product: myLib.Product;
     }
