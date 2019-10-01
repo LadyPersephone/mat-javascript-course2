171c3ca0fe18d2a6447fcfddbe467cbe35fd2b40
student <student@safebear.co.uk>
2019-10-01 10:36:52 +0100

    created types in the global d.t.s file

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for ProductList Test Automation Framework
+
+declare namespace myLib {
+    
+    //Our global 'Product' type/interface
+    interface Product {
+        name: string;
+        description: string;
+        price: string;
+    }
+}
